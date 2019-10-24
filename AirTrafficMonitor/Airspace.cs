b741a129445d929443316cf91fed41900883d914
Bertram Eriksen <djokkins@live.dk>
2019-10-24 13:57:14 +0200

    tilføjet airspace

---
+++ AirTrafficMonitor/Airspace.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitor
+{
+    class Airspace
+    {
+        public int X;
+        public int Y;
+        public int Z;
+        public int width;  //X
+        public int height; //Z
+        public int depth;  //Y
+    }
+}
