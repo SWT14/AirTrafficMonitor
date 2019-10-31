7f3e9f5c8a2b0462b716365d465a0d681709751c
au536853 <au536853@uni.au.dk>
2019-10-31 15:24:05 +0100

    .

--- AirTrafficMonitor/Interface/IAirspaceFilter.cs
+++ AirTrafficMonitor/Interface/IAirspaceFilter.cs
@@ -9,9 +9,9 @@ namespace AirTrafficMonitor
 {
     public interface IAirSpaceFilter
     {
-        string Tag { get; set; }
-        double X_Coor { get; set; }
-        double Y_Coor { get; set; }
+        string tag { get; set; }
+        double X_coor { get; set; }
+        double Y_coor { get; set; }
         double Altitude { get; set; }
         bool AirSpace { get; set; }
         bool FilterTracks(double X, double Y, double A);
