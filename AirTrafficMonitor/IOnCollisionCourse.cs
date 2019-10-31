533d6c5e4c8101803205e841e0624f8481004310
au536853 <au536853@uni.au.dk>
2019-10-31 20:10:15 +0100

    OnCollisionCourse

---
+++ AirTrafficMonitor/IOnCollisionCourse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirTrafficMonitor
+{
+    public interface IOnCollisionCourse
+    {
+        event EventHandler<OnCollisionCourse.SeperationEvent> CreateSeperation;
+
+        double Span(ITrackCalculator track1, ITrackCalculator track2)
+        void TrackInAirSpace(TrackEvent TEtracks); 
+        Dictionary<String, TrackCalculator> _tracks { get; set; }
+        
+    }
+}
