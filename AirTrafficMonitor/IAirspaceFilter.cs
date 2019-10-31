d302ed78096ce0f314c70d91cce7085e33254b1a
Enoch Alexis <enoch.a98@hotmail.com>
2019-10-31 15:26:10 +0100

    small changes

--- AirTrafficMonitor/IAirspaceFilter.cs
+++ AirTrafficMonitor/IAirspaceFilter.cs
@@ -10,8 +10,8 @@ namespace AirTrafficMonitor
     public interface IAirSpaceFilter
     {
         string Tag { get; set; }
-        double X_Coor { get; set; }
-        double Y_Coor { get; set; }
+        double X_coor { get; set; }
+        double Y_coor { get; set; }
         double Altitude { get; set; }
         bool AirSpace { get; set; }
         bool FilterTracks(double X, double Y, double A);
