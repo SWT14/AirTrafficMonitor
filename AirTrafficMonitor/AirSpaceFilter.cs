96519e5a18f72a9b13e7d861f70fb737e1c2c3f8
Enoch Alexis <enoch.a98@hotmail.com>
2019-10-31 19:37:33 +0100

    Airspace

--- AirTrafficMonitor/AirSpaceFilter.cs
+++ AirTrafficMonitor/AirSpaceFilter.cs
@@ -13,17 +13,50 @@ namespace AirTrafficMonitor
     public class AirSpaceFilter : IAirSpaceFilter
     {
       
-        public Dictionary<string,ITrack> TrackDic { get; set; }
-        public Dictionary<string,ITrackCalculator> TrackCalcDic { get; set; }
+        public Dictionary<string,ITrack> TrackDict { get; set; }
+        public Dictionary<string,ITrackCalculator> TrackCalcDict { get; set; }
         public EventHandler<TrackinAirEvent> TrackUpdated;
         public List<ITrack> trackList { get; set; }
 
         public AirSpaceFilter(ITrackHandler trackhandler)
         {
             //Attaching the event to the constructor
+            trackhandler.trackCreated += onTrackCreated;
+            trackList = new List<ITrack>();
+            TrackDict = new Dictionary<string, ITrack>();
+            TrackCalcDict = new Dictionary<string, ITrackCalculator>();
         }
 
+        public void onTrackCreated(object s, TrackEvent trackList)
+        {
+            Tracks = trackList.tracks;
+            foreach (ITrack track in trackList)
+            {
+                if (TrackDict.ContainsKey(track.tag))
+                {
+                    if (TrackDict.Airspace)
+                    {
+                        Create(track);
+                    }
+                    else
+                    {
+                        TrackDict[track.tag] = track;
+                    }
+                }
+                else
+                {
+                    TrackDict.Add(track.tag, track);
+                }
+            }
+        }
 
+        public void Create(ITrack track)
+        {
+            if (TrackDict.ContainsKey(track.tag))
+            {
+                TrackDict[track.tag] = new 
+            }
+        }
 
         public class TrackinAirEvent : EventArgs
         {
