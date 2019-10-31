0b7ff1fda6b1cf9326f9f813ea89dd640fca6ed9
Enoch Alexis <enoch.a98@hotmail.com>
2019-10-31 18:24:02 +0100

    Airspacefilter

--- AirTrafficMonitor/AirSpaceFilter.cs
+++ AirTrafficMonitor/AirSpaceFilter.cs
@@ -7,13 +7,29 @@ using System.Threading.Tasks;
 
 
 
+
 namespace AirTrafficMonitor
 {
     public class AirSpaceFilter : IAirSpaceFilter
     {
       
+        public Dictionary<string,ITrack> TrackDic { get; set; }
+        public Dictionary<string,ITrackCalculator> TrackCalcDic { get; set; }
+        public EventHandler<TrackinAirEvent> TrackUpdated;
+        public List<ITrack> trackList { get; set; }
 
-        
+        public AirSpaceFilter(ITrackHandler trackhandler)
+        {
+            //Attaching the event to the constructor
+        }
+
+
+
+        public class TrackinAirEvent : EventArgs
+        {
+            public Dictionary<string, ITrackCalculator> tracks { get; set; }
+        }
+
 
     }
 }
